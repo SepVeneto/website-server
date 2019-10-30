f0637ae12a4493e1da042f20b4857907a19b95f1
祁恩泽 <qienze@beyondcent.com>
2019-10-30 14:15:51 +0800

    放弃格式化

--- src/router/index.ts
+++ src/router/index.ts
@@ -55,9 +55,6 @@ router.get('/article/getArticles', async (req, res) => {
     if (err) {
       response(res);
     }
-    article.forEach(item => {
-      item.columns = (item.columns as Array<string>).join(',');
-    })
     const result = {
       total,
       list: article,
