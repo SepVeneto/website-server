54b16af3d8bc9ba94dfbcae207f0645f712facb8
祁恩泽 <qienze@beyondcent.com>
2019-11-01 17:32:16 +0800

    分类标签可修改颜色

--- src/models/columns.ts
+++ src/models/columns.ts
@@ -3,11 +3,13 @@ import mongoose from 'mongoose';
 export type ColumnsDocument = mongoose.Document & {
   name: string,
   value: string,
+  color: string,
 }
 
 const columnsSchema = new mongoose.Schema({
   name: { type: String, require: true, unique: true },
   value: { type: String, require: true, unique: true },
+  color: {type: String, require: true},
 })
 
 export const Columns = mongoose.model<ColumnsDocument>('Columns', columnsSchema);
