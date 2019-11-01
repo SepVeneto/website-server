ac045ad636af76fbab5dd75bfac0c35ba80c8733
祁恩泽 <qienze@beyondcent.com>
2019-11-01 17:47:39 +0800

    背景和字体颜色

--- src/models/columns.ts
+++ src/models/columns.ts
@@ -3,13 +3,15 @@ import mongoose from 'mongoose';
 export type ColumnsDocument = mongoose.Document & {
   name: string,
   value: string,
-  color: string,
+  background: string,
+  font: string,
 }
 
 const columnsSchema = new mongoose.Schema({
   name: { type: String, require: true, unique: true },
   value: { type: String, require: true, unique: true },
-  color: {type: String, require: true},
+  background: {type: String},
+  font: {type: String}
 })
 
 export const Columns = mongoose.model<ColumnsDocument>('Columns', columnsSchema);
