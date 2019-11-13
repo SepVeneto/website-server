e293f53d47524323d87353ed6eb0a6843843cab3
祁恩泽 <qienze@beyondcent.com>
2019-11-13 18:21:28 +0800

    update:修改背景颜色，字体颜色通过算法计算

--- src/models/columns.ts
+++ src/models/columns.ts
@@ -3,15 +3,13 @@ import mongoose from 'mongoose';
 export type ColumnsDocument = mongoose.Document & {
   name: string,
   value: string,
-  background: string,
-  font: string,
+  color: string,
 }
 
 const columnsSchema = new mongoose.Schema({
   name: { type: String, require: true, unique: true },
   value: { type: String, require: true, unique: true },
-  background: {type: String},
-  font: {type: String}
+  color: {type: String},
 })
 
 export const Columns = mongoose.model<ColumnsDocument>('Columns', columnsSchema);
